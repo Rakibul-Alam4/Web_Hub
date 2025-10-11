13da491277ba5e9ee2404c566e775b116b06af71
Rakibul Alam <91847242+Rakibul-Alam4@users.noreply.github.com>
2025-10-11 11:01:17 +0600

    Add files via upload

---
+++ HotelDreamSTS/HotelDreamSTS/HotelDreamSTS/ManagerPage0.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace HotelDreamSTS
+{
+    partial class ManagerPage0
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoginPage = new System.Windows.Forms.Button();
+            this.btnBill = new System.Windows.Forms.Button();
+            this.btnServices = new System.Windows.Forms.Button();
+            this.btnFood = new System.Windows.Forms.Button();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.btnRoom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoginPage
+            // 
+            this.btnLoginPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoginPage.Location = new System.Drawing.Point(163, 396);
+            this.btnLoginPage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLoginPage.Name = "btnLoginPage";
+            this.btnLoginPage.Size = new System.Drawing.Size(440, 69);
+            this.btnLoginPage.TabIndex = 17;
+            this.btnLoginPage.Text = "Log out";
+            this.btnLoginPage.UseVisualStyleBackColor = true;
+            this.btnLoginPage.Click += new System.EventHandler(this.btnLoginPage_Click_1);
+            // 
+            // btnBill
+            // 
+            this.btnBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBill.Location = new System.Drawing.Point(163, 320);
+            this.btnBill.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBill.Name = "btnBill";
+            this.btnBill.Size = new System.Drawing.Size(440, 69);
+            this.btnBill.TabIndex = 16;
+            this.btnBill.Text = "Bill History";
+            this.btnBill.UseVisualStyleBackColor = true;
+            this.btnBill.Click += new System.EventHandler(this.btnBill_Click_1);
+            // 
+            // btnServices
+            // 
+            this.btnServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnServices.Location = new System.Drawing.Point(163, 244);
+            this.btnServices.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnServices.Name = "btnServices";
+            this.btnServices.Size = new System.Drawing.Size(440, 69);
+            this.btnServices.TabIndex = 15;
+            this.btnServices.Text = "Other Service";
+            this.btnServices.UseVisualStyleBackColor = true;
+            this.btnServices.Click += new System.EventHandler(this.btnServices_Click_1);
+            // 
+            // btnFood
+            // 
+            this.btnFood.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFood.Location = new System.Drawing.Point(163, 167);
+            this.btnFood.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFood.Name = "btnFood";
+            this.btnFood.Size = new System.Drawing.Size(440, 69);
+            this.btnFood.TabIndex = 14;
+            this.btnFood.Text = "Food Menu";
+            this.btnFood.UseVisualStyleBackColor = true;
+            this.btnFood.Click += new System.EventHandler(this.btnFood_Click_1);
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomer.Location = new System.Drawing.Point(163, 91);
+            this.btnCustomer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(440, 69);
+            this.btnCustomer.TabIndex = 13;
+            this.btnCustomer.Text = "Customer Details";
+            this.btnCustomer.UseVisualStyleBackColor = true;
+            this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click_1);
+            // 
+            // btnRoom
+            // 
+            this.btnRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoom.Location = new System.Drawing.Point(163, 15);
+            this.btnRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRoom.Name = "btnRoom";
+            this.btnRoom.Size = new System.Drawing.Size(440, 69);
+            this.btnRoom.TabIndex = 12;
+            this.btnRoom.Text = "Room Details";
+            this.btnRoom.UseVisualStyleBackColor = true;
+            this.btnRoom.Click += new System.EventHandler(this.btnRoom_Click_1);
+            // 
+            // ManagerPage0
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(795, 551);
+            this.Controls.Add(this.btnLoginPage);
+            this.Controls.Add(this.btnBill);
+            this.Controls.Add(this.btnServices);
+            this.Controls.Add(this.btnFood);
+            this.Controls.Add(this.btnCustomer);
+            this.Controls.Add(this.btnRoom);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ManagerPage0";
+            this.Text = "ManagerPage0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ManagerPage0_FormClosed);
+            this.Load += new System.EventHandler(this.ManagerPage0_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoginPage;
+        private System.Windows.Forms.Button btnBill;
+        private System.Windows.Forms.Button btnServices;
+        private System.Windows.Forms.Button btnFood;
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.Button btnRoom;
+
+    }
+}
